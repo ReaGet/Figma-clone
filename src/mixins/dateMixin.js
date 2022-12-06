5a10f7098807f5a6d8fbc044ac381c8d07034ec8
Rifat <reaget.acc@gmail.com>
2022-12-06 20:48:31 +0300

    started creating time ellapsed functionality

--- src/mixins/dateMixin.js
+++ src/mixins/dateMixin.js
@@ -17,5 +17,27 @@ export default {
 
       return new Intl.DateTimeFormat("ru-RU", options).format(new Date(value));
     },
+    timeElapsed(value) {
+      const time = {};
+      const current = new Date();
+      const diff = current - value;
+      time.years = current.getFullYear();
+      time.months = ~~(
+        diff.getFullYear() * 12 -
+        current.getMonth() +
+        value.getMonth()
+      );
+      time.weeks = ~~(diff / 604800000);
+      time.days = ~~(diff / 86400000);
+      time.minutes = ~~(this.diff / 86400000);
+      time.names = {
+        year: ["год", "года", "лет"],
+        month: ["месяц", "месяца", "месяцев"],
+        day: ["день", "дня", "дней"],
+        hour: ["час", "часа", "часов"],
+        minute: ["минута", "минуты", "минут"],
+        second: ["секунда", "секунды", "секунд"],
+      };
+    },
   },
 };
